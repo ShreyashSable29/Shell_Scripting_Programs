a1b55a03c2703418e0b002b0714434e214743724
Shreyash Sable <125114741+ShreyashSable29@users.noreply.github.com>
2023-03-22 19:20:29 +0530

    Add files via upload

---
+++ Day-7/Array/SumOfIntergersZero.sh
@@ -0,0 +1,14 @@
+# Write a Program to show Sum of three Integer adds to ZERO
+# Take three integers as input
+echo "Enter three integers: "
+read a
+read b
+read c
+
+# Check if the sum of the integers is equal to zero
+if [[ $((a + b + c)) -eq 0 ]]
+then
+    echo "The sum of the three integers is zero."
+else
+    echo "The sum of the three integers is not zero."
+fi
